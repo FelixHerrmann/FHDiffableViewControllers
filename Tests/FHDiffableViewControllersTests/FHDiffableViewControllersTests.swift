84de638d0950f9f7c76ba7ad1a04932292f43445
Felix Herrmann <felix.herrmann@icloud.com>
2020-06-12 03:21:15 +0200

    Test removed

--- Tests/FHDiffableViewControllersTests/FHDiffableViewControllersTests.swift
+++ Tests/FHDiffableViewControllersTests/FHDiffableViewControllersTests.swift
@@ -6,7 +6,6 @@ final class FHDiffableViewControllersTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-//        XCTAssertEqual(FHDiffableViewControllers().text, "Hello, World!")
     }
 
     static var allTests = [
