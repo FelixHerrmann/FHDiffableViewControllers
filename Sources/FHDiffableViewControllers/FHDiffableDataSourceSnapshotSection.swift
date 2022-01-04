4b4106c7ed9677d7e432b1bfb282c14de7c42ca2
Felix Herrmann <felix.herrmann@icloud.com>
2022-01-04 23:16:33 +0100

    added item result builder

--- Sources/FHDiffableViewControllers/FHDiffableDataSourceSnapshotSection.swift
+++ Sources/FHDiffableViewControllers/FHDiffableDataSourceSnapshotSection.swift
@@ -6,7 +6,7 @@
 //
 
 /// A representation of the **UITableView** or **UICollectionView** sections data.
-public struct FHDiffableDataSourceSnapshotSection<SectionIdentifierType: Hashable, ItemIdentifierType: Hashable> {
+public struct FHDiffableDataSourceSnapshotSection<SectionIdentifierType: Hashable, ItemIdentifierType: Hashable>: Hashable {
     
     /// The identifier of the section.
     public var sectionIdentifier: SectionIdentifierType
@@ -22,5 +22,22 @@ public struct FHDiffableDataSourceSnapshotSection<SectionIdentifierType: Hashabl
     public init(_ sectionIdentifier: SectionIdentifierType, items itemIdentifiers: [ItemIdentifierType]) {
         self.sectionIdentifier = sectionIdentifier
         self.itemIdentifiers = itemIdentifiers
+    }
+}
+
+
+// MARK: - Item Builder
+
+extension FHDiffableDataSourceSnapshotSection {
+    
+    /// A typealias for ``FHDiffableDataSourceSnapshotItemBuilder`` with the item identifier type.
+    public typealias FHItemBuilder = FHDiffableDataSourceSnapshotItemBuilder<ItemIdentifierType>
+    
+    /// Instantiates an instance of ``FHDiffableDataSourceSnapshotSection`` with a result builder.
+    /// - Parameters:
+    ///   - sectionIdentifier: The identifier of the section.
+    ///   - itemBuilder: A builder to build the item identifiers for the section.
+    public init(_ sectionIdentifier: SectionIdentifierType, @FHItemBuilder itemBuilder: () throws -> [ItemIdentifierType]) rethrows {
+        self.init(sectionIdentifier, items: try itemBuilder())
     }
 }
